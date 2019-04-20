53ad9dafea81e9d5e512f1b5c40c11458c69236b
Vivek Malhan <vivek.malhan@yahoo.com>
2019-04-20 00:38:56 -0400

    added projects crousel

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'; 
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
 <BrowserRouter>
